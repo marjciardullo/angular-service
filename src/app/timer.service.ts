3c25d55dc8d0e597d606e572e876caab5698c5db
marjciardullo <marjoryeramirez@gmail.com>
2022-04-01 19:29:06 -0300

    1 file modified

--- src/app/timer.service.ts
+++ src/app/timer.service.ts
@@ -2,7 +2,24 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class TimerService {
-  
-  constructor() { }
+  private timer: any;
+  private counter = 0;
 
+  constructor() {}
+
+  start(ms: number) {
+    if (!this.timer) {
+      this.timer = setInterval(() => {
+        this.counter++;
+      }, ms);
+    }
+  }
+
+  stop() {
+    
+  }
+
+  getCount() {
+    return this.counter;
+  }
 }
